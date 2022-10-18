fe9a1ece8139870bae16b13461f0a42319cd26a4
fazziclay <fazziclay@gmail.com>
2022-10-18 17:17:06 +0300

    init commit

---
+++ src/ru/fazziclay/opentoday/telemetry/OpenTodayTelemetry.java
@@ -0,0 +1,6 @@
+package ru.fazziclay.opentoday.telemetry;
+
+public class OpenTodayTelemetry {
+    public static final String VERSION_NAME = "v1.11";
+    public static final int VERSION_CODE = 11;
+}
